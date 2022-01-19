b16356ee7d53e6c82481faff8cad323393321e78
Donizete Crisostomo <donizete112@gmail.com>
2022-01-18 23:01:41 -0300

    adicionar

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
-
-namespace demo
+﻿
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
+    static int Adicionar20(int z)
         {
-            Console.WriteLine("Hello World!");
+           return z+20;
         }
-    }
+    
+  public static void Main()
+  {
+      int a =2;
+    a=  Adicionar20(a);
+    System.Console.WriteLine($"O valor da variavel é {a}");
+  }
 }
